7235620903400d3a0def052a3c639563a1e8d7f6
Ivan_Sagir <klassnayazhizn14@gmail.com>
2022-09-08 20:44:57 +0200

    dd

--- banery.js
+++ banery.js
@@ -1,1 +1,22 @@
-
+function calculation() {
+    const num1 = Number(document.getElementById('num1').value);
+    const num2 = Number(document.getElementById('num2').value);
+    switch (cl) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+  
+      default:
+    }
+  
+    document.getElementById('result').innerHTML = result;
+  }
